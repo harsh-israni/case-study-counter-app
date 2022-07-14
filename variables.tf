6ea19a29c778893a1d2e106dcf27eca0f6bb3317
Harsh Israni <103442055+harsh-israni@users.noreply.github.com>
2022-07-14 17:39:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,23 @@ variable "environment" {
   description = "AWS environment"
   default = "prod"
 }
+
+variable "vpc_cidr" {
+  description = "cidr range of vpc"
+  default = "prod"
+}
+
+variable "public_subnets_cidr" {
+  description = "cidr block for public subnet"
+  default = "prod"
+}
+
+variable "private_subnets_cidr" {
+  description = "cidr block for private subnet"
+  default = "prod"
+}
+
+variable "availability_zones" {
+  description = "AWS availability zones"
+  default = "prod"
+}
